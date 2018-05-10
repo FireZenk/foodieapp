bdd8b713bf03e68db579b603502ffbb8cfb40138
firezenk <firezenk@gmail.com>
2018-05-10 21:22:26 +0200

    Create the MapScreen and its Presenter

--- app/src/main/java/org/firezenk/foodieapp/di/AppComponent.kt
+++ app/src/main/java/org/firezenk/foodieapp/di/AppComponent.kt
@@ -2,10 +2,11 @@ package org.firezenk.foodieapp.di
 
 import dagger.Component
 import org.firezenk.foodieapp.di.module.AppModule
+import org.firezenk.foodieapp.ui.features.map.MapScreen
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [AppModule::class])
 interface AppComponent {
-
+    infix fun inject(mapScreen: MapScreen)
 }
---
+++ app/src/main/java/org/firezenk/foodieapp/ui/features/map/MapScreen.kt
@@ -0,0 +1,33 @@
+package org.firezenk.foodieapp.ui.features.map
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.firezenk.foodieapp.App
+import org.firezenk.foodieapp.App.Companion.component
+import org.firezenk.foodieapp.R
+import org.firezenk.foodieapp.ui.features.commons.Screen
+import javax.inject.Inject
+
+class MapScreen : AppCompatActivity(), Screen<States> {
+
+    @Inject lateinit var presenter: MapPresenter
+    @Inject lateinit var actions: MapActions
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.screen_map)
+
+        component inject this
+
+        presenter init this
+    }
+
+    override fun onDestroy() {
+        presenter.destroy()
+        super.onDestroy()
+    }
+
+    override fun render(state: States) {
+
+    }
+}
