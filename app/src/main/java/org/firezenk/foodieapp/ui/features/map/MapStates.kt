73f7429b0203be37ece520c2bd9c1e073e81f235
firezenk <firezenk@gmail.com>
2018-05-12 17:24:04 +0200

    Add states to show the user pointer, venues and possible errors

--- app/src/main/java/org/firezenk/foodieapp/ui/features/map/MapStates.kt
+++ app/src/main/java/org/firezenk/foodieapp/ui/features/map/MapStates.kt
@@ -1,13 +1,19 @@
 package org.firezenk.foodieapp.ui.features.map
 
+import org.firezenk.foodieapp.domain.models.Venue
 import org.firezenk.foodieapp.ui.features.commons.State
 import javax.inject.Inject
 
 class MapStates @Inject constructor() {
 
     sealed class MapState : State() {
-
+        class PositionReady(val lat: Double, val lng: Double) : MapState()
+        class PointersReady(val venues: List<Venue>) : MapState()
+        class ErrorMessage(val e: Throwable) : MapState()
     }
 }
 
 typealias States = MapStates.MapState
+typealias PositionReady = MapStates.MapState.PositionReady
+typealias PointersReady = MapStates.MapState.PointersReady
+typealias ErrorMessage = MapStates.MapState.ErrorMessage
