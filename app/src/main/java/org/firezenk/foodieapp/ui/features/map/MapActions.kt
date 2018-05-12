2299356e23a3712d98aa9cf44dd77ea74596b269
firezenk <firezenk@gmail.com>
2018-05-12 17:23:43 +0200

    Add action to load venues

--- app/src/main/java/org/firezenk/foodieapp/ui/features/map/MapActions.kt
+++ app/src/main/java/org/firezenk/foodieapp/ui/features/map/MapActions.kt
@@ -1,13 +1,21 @@
 package org.firezenk.foodieapp.ui.features.map
 
+import org.firezenk.foodieapp.domain.usecases.ObtainCoordinates
+import org.firezenk.foodieapp.domain.usecases.ObtainVenues
 import org.firezenk.foodieapp.ui.features.commons.Action
 import javax.inject.Inject
 
-class MapActions @Inject constructor() {
+class MapActions @Inject constructor(private val obtainVenues: ObtainVenues,
+                                     private val obtainCoordinates: ObtainCoordinates) {
+
+    fun loadVenues() = LoadVenues(obtainVenues, obtainCoordinates)
 
     sealed class MapAction : Action() {
 
+        class LoadVenues(val obtainVenues: ObtainVenues, val obtainCoordinates: ObtainCoordinates)
+            : MapAction()
     }
 }
 
 typealias Actions = MapActions.MapAction
+typealias LoadVenues = MapActions.MapAction.LoadVenues
