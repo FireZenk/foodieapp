af89234d91b096bab0b7e0377cf1b6d68215a1c8
firezenk <firezenk@gmail.com>
2018-05-12 17:26:07 +0200

    Show the markers received on the map

--- app/src/main/java/org/firezenk/foodieapp/ui/features/map/MapScreen.kt
+++ app/src/main/java/org/firezenk/foodieapp/ui/features/map/MapScreen.kt
@@ -1,27 +1,54 @@
 package org.firezenk.foodieapp.ui.features.map
 
+import android.Manifest
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapboxMap
 import kotlinx.android.synthetic.main.screen_map.*
 import org.firezenk.foodieapp.App.Companion.component
 import org.firezenk.foodieapp.R
+import org.firezenk.foodieapp.ui.extensions.grantPermissions
 import org.firezenk.foodieapp.ui.features.commons.Screen
+import org.firezenk.foodieapp.ui.features.map.di.MapModule
 import javax.inject.Inject
+import com.mapbox.mapboxsdk.annotations.IconFactory
+import org.firezenk.foodieapp.ui.extensions.setVectorIcon
+import org.firezenk.foodieapp.ui.utils.getBitmapFromVectorDrawable
 
 class MapScreen : AppCompatActivity(), Screen<States> {
 
     @Inject lateinit var presenter: MapPresenter
     @Inject lateinit var actions: MapActions
 
+    private lateinit var mapboxMap: MapboxMap
+    private val userMarker: MarkerOptions by lazy {
+        MarkerOptions().apply { setVectorIcon(this@MapScreen, R.drawable.ic_user_pin) }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.screen_map)
 
-        component inject this
+        component add MapModule(this) inject this
 
         presenter init this
 
         mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            mapboxMap = it
+            mapboxMap.setOnMarkerClickListener {
+                Snackbar.make(mapView, it.title, Snackbar.LENGTH_SHORT).show()
+                return@setOnMarkerClickListener true
+            }
+            grantPermissions(listOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION),
+                    { presenter reduce actions.loadVenues() })
+        }
     }
 
     override fun onStart() {
@@ -61,6 +88,36 @@ class MapScreen : AppCompatActivity(), Screen<States> {
     }
 
     override fun render(state: States) {
+        when (state) {
+            is PositionReady -> {
+                val position = LatLng(state.lat, state.lng)
+                if (mapboxMap.markers.size > 0) {
+                    mapboxMap.removeMarker(userMarker.marker)
+                }
+                userMarker.position = position
+                mapboxMap.addMarker(userMarker)
 
+                mapboxMap.cameraPosition = CameraPosition.Builder()
+                        .target(position)
+                        .zoom(15.0)
+                        .build()
+            }
+            is PointersReady -> {
+                val iconFactory = IconFactory.getInstance(this@MapScreen)
+                val icon = iconFactory.fromBitmap(
+                        getBitmapFromVectorDrawable(this, R.drawable.ic_food_pin))
+
+                state.venues.forEach {
+                    mapboxMap.addMarker(MarkerOptions()
+                            .position(LatLng(it.location.lat, it.location.lng))
+                            .setTitle(it.name)
+                            .setIcon(icon))
+                }
+            }
+            is ErrorMessage -> {
+                state.e.printStackTrace()
+                Snackbar.make(mapView, state.e.message ?: "error", Snackbar.LENGTH_SHORT).show()
+            }
+        }
     }
 }
