c106a8881a71545270624475a0999ce73f04f0ec
firezenk <firezenk@gmail.com>
2018-05-12 20:58:03 +0200

    Add database operations to make and cancel reservations

--- app/src/main/java/org/firezenk/foodieapp/data/db/venues/VenueDao.kt
+++ app/src/main/java/org/firezenk/foodieapp/data/db/venues/VenueDao.kt
@@ -2,6 +2,7 @@ package org.firezenk.foodieapp.data.db.venues
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Query
+import io.reactivex.Completable
 import io.reactivex.Single
 import org.firezenk.foodieapp.data.db.BaseDao
 import org.intellij.lang.annotations.Language
@@ -16,4 +17,12 @@ interface VenueDao: BaseDao<VenueEntity> {
     @Language("RoomSql")
     @Query("SELECT * FROM $VENUE_TABLE WHERE name = :venueName LIMIT 1")
     fun findVenue(venueName: String): Single<VenueEntity>
+
+    @Language("RoomSql")
+    @Query("UPDATE $VENUE_TABLE SET reserved = 1 WHERE id = :id")
+    fun makeReservation(id: String)
+
+    @Language("RoomSql")
+    @Query("UPDATE $VENUE_TABLE SET reserved = 0 WHERE id = :id")
+    fun cancelReservation(id: String)
 }
--- app/src/main/java/org/firezenk/foodieapp/data/db/venues/VenueDataSource.kt
+++ app/src/main/java/org/firezenk/foodieapp/data/db/venues/VenueDataSource.kt
@@ -1,5 +1,6 @@
 package org.firezenk.foodieapp.data.db.venues
 
+import io.reactivex.Completable
 import io.reactivex.Single
 import org.firezenk.foodieapp.domain.models.Location
 import org.firezenk.foodieapp.domain.models.Venue
@@ -17,6 +18,10 @@ class VenueDataSource @Inject constructor(private val venueDao: VenueDao) {
     fun findVenue(venueName: String): Single<Venue> {
         return venueDao.findVenue(venueName).map { mapVenue(it) }
     }
+
+    fun makeReservation(venueId: String) = venueDao.makeReservation(venueId)
+
+    fun cancelReservation(venueId: String) = venueDao.cancelReservation(venueId)
 
     private fun mapVenueEntity(it: Venue): VenueEntity {
         return VenueEntity(it.id, it.name, false, LocationEntity(it.location.address,
