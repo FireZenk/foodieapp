df6355e0e0a37eb4c5a678aed87f352746b72840
firezenk <firezenk@gmail.com>
2018-05-12 17:19:59 +0200

    Make venues from foursquare work

--- app/src/main/java/org/firezenk/foodieapp/domain/models/Venue.kt
+++ app/src/main/java/org/firezenk/foodieapp/domain/models/Venue.kt
@@ -1,7 +1,7 @@
 package org.firezenk.foodieapp.domain.models
 
-data class Venue(val id: Int, val name: String, val location: Location)
+data class Venue(val id: String, val name: String, val location: Location)
 
-data class Location(val address: String, val crossStreet: String, val city: String,
-                    val state: String, val postalCode: String, val country: String,
-                    val lat: Double, val lng: Double, val distance: Float)
+data class Location(val address: String?, val crossStreet: String?, val city: String?,
+                    val state: String?, val postalCode: String?, val country: String?,
+                    val lat: Double, val lng: Double, val distance: Float?)
