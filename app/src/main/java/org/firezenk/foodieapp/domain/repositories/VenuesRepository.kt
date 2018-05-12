d8435602004d98d14635c0fdac8ba94a745078b4
firezenk <firezenk@gmail.com>
2018-05-12 18:32:46 +0200

    Prepare to get the venue with its name

--- app/src/main/java/org/firezenk/foodieapp/domain/repositories/VenuesRepository.kt
+++ app/src/main/java/org/firezenk/foodieapp/domain/repositories/VenuesRepository.kt
@@ -1,13 +1,18 @@
 package org.firezenk.foodieapp.domain.repositories
 
 import io.reactivex.Single
-import org.firezenk.foodieapp.data.net.venues.VenueDataSource
+import org.firezenk.foodieapp.data.db.venues.VenueDataSource as Database
+import org.firezenk.foodieapp.data.net.venues.VenueDataSource as Network
 import org.firezenk.foodieapp.domain.models.Venue
 import org.firezenk.foodieapp.domain.subscribeOnIO
 import javax.inject.Inject
 
-class VenuesRepository @Inject constructor(private val dataSource: VenueDataSource) {
+class VenuesRepository @Inject constructor(private val netDataSource: Network,
+                                           private val dbDataSource: Database) {
 
     fun findNearbyVenues(lat: Double, lng: Double): Single<List<Venue>>
-            = dataSource.findNearbyVenues(lat, lng).subscribeOnIO()
+            = netDataSource.findNearbyVenues(lat, lng).subscribeOnIO()
+
+    fun findVenue(venueName: String): Single<Venue>
+            = dbDataSource.findVenue(venueName).subscribeOnIO()
 }
--- app/src/main/java/org/firezenk/foodieapp/ui/features/map/MapActions.kt
+++ app/src/main/java/org/firezenk/foodieapp/ui/features/map/MapActions.kt
@@ -1,22 +1,24 @@
 package org.firezenk.foodieapp.ui.features.map
 
 import org.firezenk.foodieapp.domain.usecases.ObtainCoordinates
+import org.firezenk.foodieapp.domain.usecases.ObtainVenue
 import org.firezenk.foodieapp.domain.usecases.ObtainVenues
 import org.firezenk.foodieapp.ui.features.commons.Action
 import javax.inject.Inject
 
 class MapActions @Inject constructor(private val obtainVenues: ObtainVenues,
-                                     private val obtainCoordinates: ObtainCoordinates) {
+                                     private val obtainCoordinates: ObtainCoordinates,
+                                     private val obtainVenue: ObtainVenue) {
 
     fun loadVenues() = LoadVenues(obtainVenues, obtainCoordinates)
-    fun openVenue(venueName: String) = OpenVenueDetail(venueName)
+    fun openVenue(venueName: String) = OpenVenueDetail(obtainVenue, venueName)
 
     sealed class MapAction : Action() {
 
         class LoadVenues(val obtainVenues: ObtainVenues, val obtainCoordinates: ObtainCoordinates)
             : MapAction()
 
-        class OpenVenueDetail(venueName: String): MapAction()
+        class OpenVenueDetail(val obtainVenue: ObtainVenue, val venueName: String): MapAction()
     }
 }
 
